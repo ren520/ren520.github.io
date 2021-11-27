98706dc79e0964f1b4d2867c7fd110d1c5b01b27
zengshangren <3032802862@qq.com>
2021-11-27 23:38:51 +0800

    Site updated: 2021-11-27 23:38:51

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -13,17 +13,18 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#FF0000"
+            "color": "#FF0000"		// 修改颜色处
         });
+      
         $("body").append($i);
         $i.animate({
             "top": y - 180,
             "opacity": 0
         },
-            3000,
-            function() {
-                $i.remove();
-            });
+      3000,
+      function() {
+          $i.remove();
+      });
     });
     setTimeout('delay()', 2000);
 });
